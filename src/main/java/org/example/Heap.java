998b875a7083e2595c93da82e8fa4c0ce783272a
1cgul <1cgul@gmail.com>
2023-09-14 10:10:27 -0400

    .

---
+++ src/main/java/org/example/Heap.java
@@ -0,0 +1,44 @@
+package org.example;
+
+public class Heap {
+    int[] arr;
+    int sizeOfArray;
+    public Heap(){
+        sizeOfArray = 10;
+        arr = new int[sizeOfArray];
+    }
+    public Heap(int[] arr, int sizeOfArray){
+        this.arr = arr;
+        this.sizeOfArray = sizeOfArray;
+    }
+    int getParentIndex(int index){
+        return (int)Math.floor((index-1)/2);
+    }
+    int getLeftChildIndex(int index){
+        return 2*index+1;
+    }
+    int getRightChildIndex(int index){
+        return 2*index+2;
+    }
+    void swap(int index1, int index2){
+        int temp;
+        temp = index1;
+        index2 = index1;
+        index1 = temp;
+    }
+    void heapifyUp(int index){
+
+    }
+    void heapifyDown(int index){
+
+    }
+    void add(int item){
+
+    }
+    void show(){
+        for(int i = 0; i < sizeOfArray; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        int sizeOfArray = 10;
+        int[] arr = new int[sizeOfArray];
+        arr[0] = 50;
+        arr[1] = 150;
+        arr[2] = 32;
+        arr[3] = 16;
+        arr[4] = 323;
+        arr[5] = 84;
+        arr[6] = 41;
+        arr[7] = 99;
+        arr[8] = 100;
+        arr[9] = 25;
 
+        Heap ex = new Heap(arr, sizeOfArray);
+        ex.show();
     }
 }
