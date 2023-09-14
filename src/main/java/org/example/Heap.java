6951dd8f6231677a78c90293c962124220ac425d
1cgul <1cgul@gmail.com>
2023-09-14 10:33:03 -0400

    .

--- src/main/java/org/example/Heap.java
+++ src/main/java/org/example/Heap.java
@@ -4,15 +4,15 @@ public class Heap {
     private int[] arr;
     private int sizeOfArray;
     public Heap(){
-        sizeOfArray = 10;
-        arr = new int[sizeOfArray];
+        sizeOfArray = 0; //int is 0 because there are no members in the array yet
+        arr = new int[10];
     }
     public Heap(int[] arr, int sizeOfArray){
         this.arr = arr;
         this.sizeOfArray = sizeOfArray;
     }
     int getParentIndex(int index){
-        return (int)Math.floor((index-1)/2);
+        return Math.floorDiv(index-1, 2);
     }
     int getLeftChildIndex(int index){
         return 2*index+1;
@@ -22,13 +22,17 @@ public class Heap {
     }
     void swap(int index1, int index2){
         int temp;
-        temp = index1;
-        index2 = index1;
-        index1 = temp;
+        temp = arr[index1];
+        arr[index2] = arr[index1];
+        arr[index1] = temp;
     }
     void heapifyUp(int index){
-        if(index == 0){
+        int parentIndex = this.getParentIndex(index);
+        while(arr[parentIndex] > arr[index]){ //keep going up the tree while out of order
+            swap(parentIndex, index);
 
+            index = parentIndex;
+            parentIndex = this.getParentIndex(index);
         }
     }
     void heapifyDown(int index){
